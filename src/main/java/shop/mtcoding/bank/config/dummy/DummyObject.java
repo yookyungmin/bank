77be3c0169c70d9cbba08f517aa76331d8f1f9d9
yookyungmin <ykmykm4608@naver.com>
2023-07-24 15:31:28 +0900

    @DataJpaTest 더미데이터 만들기, @DataJpaTest autoincrement 초기화, @DataJpaTest 더티체킹

--- src/main/java/shop/mtcoding/bank/config/dummy/DummyObject.java
+++ src/main/java/shop/mtcoding/bank/config/dummy/DummyObject.java
@@ -2,6 +2,7 @@ package shop.mtcoding.bank.config.dummy;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.transaction.Transaction;
 import shop.mtcoding.bank.domain.transaction.TransactionEnum;
 import shop.mtcoding.bank.domain.user.User;
@@ -11,6 +12,80 @@ import java.time.LocalDateTime;
 
 public class DummyObject {
 
+
+    protected Transaction newWithdrawTransaction(Account account, AccountRepository accountRepository){
+        account.withdraw(100L); // 1000원이 있다면 900원이 됨
+        //서비스레이어에서 값을 바꾸는게 아니기 떄문에 더티체킹이 안됨 직접 바꿔주어ㅑ함
+
+        //Repository Test에는 더티체킹 됨
+        //controller test에서는 더티체킹 안됨
+        //더티체킹이 안되어서 업데이트 쿼리 날리게끔 작성
+        if(accountRepository != null){
+            accountRepository.save(account);
+        }
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(account)
+                .depositAccount(null)
+                .witdrawAccountBalance(account.getBalance())
+                .depositAccountBalance(null)
+                .amount(100L)
+                .gubun(TransactionEnum.WITHDRAW)
+                .sender(account.getNumber() + "")
+                .receiver("ATN")
+                .tel("01022227777")
+                .build();
+
+        return transaction;
+
+    }
+
+    protected Transaction newTransferTransaction(Account withdrawAccount, Account depositAccount, AccountRepository accountRepository){
+        withdrawAccount.withdraw(100L); // 1000원이 있다면 900원이 됨
+        depositAccount.deposit(100L);
+        //서비스레이어에서 값을 바 꾸는게 아니기 떄문에 더티체킹이 안됨 직접 바꿔주어ㅑ함
+
+        if(accountRepository != null){
+            accountRepository.save(withdrawAccount);
+            accountRepository.save(depositAccount);
+        }
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(withdrawAccount)
+                .depositAccount(depositAccount)
+                .witdrawAccountBalance(withdrawAccount.getBalance())
+                .depositAccountBalance(depositAccount.getBalance())
+                .amount(100L)
+                .gubun(TransactionEnum.WITHDRAW)
+                .sender(withdrawAccount.getNumber() + "")
+                .receiver(depositAccount.getNumber() + "")
+                .tel("01022227777")
+                .build();
+
+        return transaction;
+
+    }
+
+    protected Transaction newDepositTransaction(Account account, AccountRepository accountRepository){
+        account.deposit(100L); // 1000원이 있다면 900원이 됨
+        //서비스레이어에서 값을 바꾸는게 아니기 떄문에 더티체킹이 안됨 직접 바꿔주어ㅑ함
+
+        if(accountRepository != null){
+            accountRepository.save(account);
+        }
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(null)
+                .depositAccount(account)
+                .witdrawAccountBalance(null)
+                .depositAccountBalance(account.getBalance())
+                .amount(100L)
+                .gubun(TransactionEnum.DEPOSIT)
+                .sender("ATM")
+                .receiver(account.getNumber() + "")
+                .tel("01022227777")
+                .build();
+
+        return transaction;
+
+    }
 
     //계좌 1111L 1000원
     //입금 트랜잭션 -> 1100원 변경 -> 입금 트랜잭션 히스토리가 생성되어야 함
--- src/main/java/shop/mtcoding/bank/domain/transaction/TransactionRepositoryImpl.java
+++ src/main/java/shop/mtcoding/bank/domain/transaction/TransactionRepositoryImpl.java
@@ -8,11 +8,12 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 interface Dao{
-    List<Transaction> findTransactionList(@Param("accountid") Long accountiD, @Param("gubun") String gubun,
+    List<Transaction> findTransactionList(@Param("accountId") Long accountId, @Param("gubun") String gubun,
                                           @Param("page") Integer page);
 }
 //파라미터 여러개값이@param 붙여야함
 //TransactionRepository에 만들면 쿼리는 짤수 있는데 동적쿼리는불가해서 인터페이스로 만듬
+
 
 //impl 필수, TransactionRepository가 앞에 붙어야 한다.
 @RequiredArgsConstructor
@@ -27,19 +28,19 @@ public class TransactionRepositoryImpl implements  Dao {
         //동적쿼리(gubun 값을 가지고 동적쿼리 = DEPOSIOT, WITHDRAW, ALL)
         //JPQL
         String sql = "";
-        sql += "select t from Transaction t";
+        sql += "select t from Transaction t ";
 
         if(gubun.equals("WITHDRAW")){
-            sql += "join fetch t.withdrawAccount wa";
+            sql += "join fetch t.withdrawAccount wa ";
             sql += "where t.withdrawAccount.id = :withdrawAccountId";
         }else if(gubun.equals("DEPOSIT")){
-            sql += "join fetch t.depositAccount da";
+            sql += "join fetch t.depositAccount da ";
             sql += "where t.depositAccount.id = :depositAccountId";
         }else { //null
-            sql += "left join fetch t.withdrawAccount wa";
-            sql += "left join fetch t.depositAccount da";
-            sql += "where t.withdrawAccount.id = :withdrawAccountId";
-            sql += "or";
+            sql += "left join fetch t.withdrawAccount wa ";
+            sql += "left join fetch t.depositAccount da ";
+            sql += "where t.withdrawAccount.id = :withdrawAccountId ";
+            sql += "or ";
             sql += "t.depositAccount.id = :depositAccountId";
         }
 
@@ -52,7 +53,6 @@ public class TransactionRepositoryImpl implements  Dao {
         }else{
             query = query.setParameter("withdrawAccountId", accountId);
             query = query.setParameter("depositAccountId", accountId);
-
         }
 
         query.setFirstResult(page*5); // 5, 10, 15
